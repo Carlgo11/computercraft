c605c1164b79327b21bd90450e33df9f27c7551b
Carlgo11 <github@carlgo11.com>
2016-03-08 00:42:34 +0100

    Add server side php script for the Door code.

---
+++ Door/Server script/config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    # database settings
+    'mysql-host' => 'localhost',
+    'mysql-username' => 'computercraft',
+    'mysql-password' => 'password',
+    'mysql-database' => 'computercraft',
+);
+?>
---
+++ Door/Server script/index.php
@@ -0,0 +1,37 @@
+<?php
+
+global $config;
+
+function getAccessLevel($table, $user) {
+    $config = include 'config.php';
+    $database = mysqli_connect($config['mysql-host'], $config['mysql-username'], $config['mysql-password'], $config['mysql-database']);
+    $query = $database->prepare("SELECT `accesslevel` FROM `" . $table . "` WHERE `user` = ?");
+    $query->bind_param("s", $user);
+    $query->execute();
+    $query->bind_result($accesslevel);
+    $query->fetch();
+    if (isset($accesslevel)) {
+        die("$accesslevel");
+    } else {
+        die("0");
+    }
+}
+
+function main() {
+    $user = NULL;
+    $tablpe = NULL;
+    if (isset($_GET['player'])) {
+        $user = $_GET['player'];
+        if (isset($_GET['group'])) {
+            $table = $_GET['group'];
+            getAccessLevel(md5($table), $user);
+        } else {
+            die("group param isn't set.");
+        }
+    } else {
+        die("player param isn't set.");
+    }
+}
+
+main();
+?>
