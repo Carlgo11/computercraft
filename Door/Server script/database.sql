21d38e24dc8b0b33ce424fed9e37c3264395fdf7
Carlgo11 <Carlgo11@users.noreply.github.com>
2016-03-27 19:42:02 +0200

    Create database.sql

---
+++ Door/Server script/database.sql
@@ -0,0 +1,7 @@
+-- Creates table called '098f6bcd4621d373cade4e832627b4f6' which is a md5 hash of 'test'.
+
+CREATE TABLE `098f6bcd4621d373cade4e832627b4f6` (
+ `user` varchar(16) NOT NULL,
+ `accesslevel` int(11) NOT NULL,
+ PRIMARY KEY (`user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
