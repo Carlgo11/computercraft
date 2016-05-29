fd7e9d8101225b8771e62744b3c34635c042e85a
Carlgo11 <Carlgo11@users.noreply.github.com>
2016-05-29 03:04:37 +0200

    Update place script

--- Place/place.lua
+++ Place/place.lua
@@ -1,32 +1,92 @@
-args = {...}
-if #args ~= 2 then
-    print("Usage: <command> <x> <z>")
+local args = {...}
+local delay = 20
+local direction -- 0 = Right, 1 = Left
+
+function checkArgs()
+    if #args == 3 then
+        if string.lower(args[3]) == "left" then
+            direction = 1
+        elseif string.lower(args[3]) == "right" then
+            direction = 0
+        end
+    else
+        print("Usage: <command> <x> <z> <direction>")
+        return 1
+    end
+end
+
+function checkFuel()
+    while turtle.getFuelLevel() > minFuelLevel do
+        turtle.refuel()
+        turtle.select(turtle.getSelectedSlot()+1)
+    end
+end
+
+function turnLeft()
+    turtle.turnRight()
+    turtle.forward()
+    turtle.turnRight()
+end
+
+function turnRight()
+    turtle.turnLeft()
+    turtle.forward()
+    turtle.turnLeft()
+end
+
+function doNormalTurn(x)
+    if x % 2 == 0 then
+        if direction == 0 then
+            turnRight()
+        else
+            turnLeft()
+        end
+    else
+        if direction == 0 then
+            turnLeft()
+        else
+            turnRight()
+        end
+    end
+end
+
+function doFinalTurn(x)
+    turtle.turnRight()
+    turtle.turnRight()
+    print("Done!")
+end
+
+if checkArgs() then
     return
 end
+
 turtle.select(1)
-for x=0, args[2] do
+for x=0, (args[2]-1) do
     for z=1, args[1] do
-        while turtle.getItemCount(turtle.getSelectedSlot()) == 0 do
+        while turtle.getItemCount() == 0 do
             if turtle.getSelectedSlot() == 16 then
                 print("No items left.")
-                print("Waiting 20 seconds...")
-                sleep(20)
+                print("Waiting "..delay.." seconds...")
+                sleep(delay)
                 turtle.select(1)
             else
                 turtle.select(turtle.getSelectedSlot()+1)
             end
         end
         turtle.placeDown()
-        turtle.forward()
+        if z < tonumber(args[1]) then
+            if turtle.detect() then
+                if not turtle.dig() then
+                    print("Can't go forward.")
+                    return
+                end
+            end
+            turtle.forward()
+        end
     end
-    if x % 2 == 0 then
-        turtle.turnRight()
-        turtle.forward()
-        turtle.turnRight()
+    if x < tonumber(args[2]-1) then
+        doNormalTurn(x)
     else
-        turtle.turnLeft()
-        turtle.forward()
-        turtle.turnLeft()
+        doFinalTurn(x)
     end
-    turtle.forward()
 end
